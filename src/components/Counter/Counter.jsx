6e960594a427948b8d4d710cd1c3c987dd64a005
ClaytonEduard <clayton_eduard@hotmail.com>
2024-06-08 15:20:15 -0300

    Finalizando, estudo de estados do ciclo de vida do componente

---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+export class Counter extends React.Component {
+
+    constructor() {
+        super()
+        this.state = { contador: 0 }
+        console.log('Construrindo a classe counter')
+
+    }
+    UNSAFE_componentWillMount() {
+        console.log("O nosso componente contador será montado!")
+    }
+
+    componentDidMount() {
+        console.log('O componente foi montado! ');
+    }
+
+    // sempre que tiver uma mudança de props ou estado
+    shouldComponentUpdate() {
+        // return true;
+        if (this.state.contador === 2) return false;
+        return true
+    }
+
+    UNSAFE_componentWillUpdate() {
+        console.log('O componente será atualiazado! ');
+    }
+
+    // componente atualizou
+    componentDidUpdate() {
+        console.log('O componente Atualizou! ');
+    }
+
+    // componente vai desmontar
+    componentWillUnmount() {
+        console.log('O componente será desmonstado! ');
+    }
+
+    render() {
+        console.log('Renderizando o nosso componente!')
+        return (
+            <div>
+                <h1>{this.state.contador}</h1>
+                <button onClick={() => this.setState({ contador: this.state.contador - 1 })}>Diminuir</button>
+                <button onClick={() => this.setState({ contador: this.state.contador + 1 })}>Aumentar</button>
+            </div>
+        )
+    }
+
+}
